b75e9646890f21912e1c373416714c946f5d1e45
Anna Peterson <reveragb@gmail.com>
2022-07-11 12:43:06 -0600

    add array methods

--- arrays.js
+++ arrays.js
@@ -1,3 +1,9 @@
 var friends  = ["Anna", "Renae", "Cory", "Landon"];
 var ages = [31, 32, 24, 38]
 var partyMember = [true, true, true, true]
+friends.push("Jon");
+//This array will add Jon to the last place in friends
+partyMember[3] = false;
+//This array will change the last element in partyMember from true to false
+ages.pop();
+//This array will remove the last element in ages
